415808734447f97b54bb48f8c9fa13ba2eea1208
nickm <nickmangarella89@gmail.com>
2021-02-17 20:52:33 -0500

    added data to html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+// Loop through `data` and console.log each UFO sighting object
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+
+    // Append one table row for each UFO sighting object
+    var row = tbody.append("tr");
+
+    // Console.log each UFO sighting value and append 1 cell per UFO sighting value
+    Object.entries(ufoSighting).forEach(function([key,value]) {
+        var cell = row.append("td");
+
+        // Update each cell's text with UFO sighting values
+        cell.text(value);
+    });
+});
