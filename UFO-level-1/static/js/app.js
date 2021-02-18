af2f54af7ddade837f448c068cf166ffabe24452
nickm <nickmangarella89@gmail.com>
2021-02-18 18:22:41 -0500

    creating filter for date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,4 @@
-// from data.js
+// From data.js
 var tableData = data;
 
 // Get a reference to the table body
@@ -22,3 +22,33 @@ data.forEach(function(ufoSighting) {
         cell.text(value);
     });
 });
+
+// Select the form and button
+var form = d3.select("#form");
+var button = d3.select("#button");
+
+// Event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Function to run both events
+function runEnter() {
+    
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(table => table.datetime === inputValue);
+
+    console.log(filteredData);
+}
+
+
